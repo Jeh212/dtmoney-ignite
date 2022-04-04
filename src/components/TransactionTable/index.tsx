5aa41126ab4a460b26a52b6aa298c0b2c91dabd2
Jean carlos <jeandub1@hotmail.com>
2022-04-04 09:34:57 -0400

    info table

---
+++ src/components/TransactionTable/index.tsx
@@ -0,0 +1,52 @@
+import { Container } from "./styles";
+
+
+
+
+function TransactionTable() {
+
+
+
+    return (
+
+        <Container>
+
+            <table>
+
+                <thead>
+                    <tr>
+                        <th>Título</th>
+                        <th>Preço</th>
+                        <th>Categoria</th>
+                        <th>Data</th>
+
+                    </tr>
+                </thead>
+
+                <tbody>
+                    <tr className="deposit">
+
+                        <td>Desenvolvimento de Site</td>
+                        <td>12.000</td>
+                        <td>Venda</td>
+                        <td>14/03/2022</td>
+                    </tr>
+                    <tr className="withdraw">
+                        <td>Desenvolvimento de Site</td>
+                        <td>12.000</td>
+                        <td>Venda</td>
+                        <td>14/03/2022</td>
+                    </tr>
+                </tbody>
+
+            </table>
+
+
+        </Container>
+
+
+
+    )
+
+}
+export { TransactionTable };
